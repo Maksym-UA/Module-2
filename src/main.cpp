e8a21f9238b92fac5dcd4c81571453c2cf9b9446
Maksym-UA <max.savin3@gmail.com>
2026-03-14 16:25:13 +0200

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,66 @@
 #include <Arduino.h>
 
+//Led state
+enum class LedState : uint8_t {
+  Off = LOW,
+  On = HIGH
+};
+
 //Configure parameters
-static constexpr uint8_t RelayPin = 17;
-static constexpr uint8_t SensorPin = 16;
-static constexpr uint32_t BaudRate = 115200;
+// Configuration parameters
+struct Config {
+  static constexpr uint8_t Led1 = 16;
+  static constexpr uint8_t Led2 = 17;
+  static constexpr uint8_t Led3 = 18;
+  static constexpr uint32_t BaudRate = 115200;
+};
 
-static int TotalIterations = 15;
-
+static int  TotalIterations = 15;
 volatile unsigned long StartTime = 0;
 volatile unsigned long EndTime = 0;
-volatile bool IsMeasuring = false;
+volatile bool isBlinking = false;
 
 unsigned long TotalDuration = 0;
 int count = 0;
 
+// Class to control the LED
+class Led {
+  private:
+    const uint8_t pin;
+  public:
+    explicit constexpr Led(uint8_t pinNumber) : pin(pinNumber) {}
+
+    void init() const {
+      pinMode(pin, OUTPUT);
+    }
+
+    void set(LedState state) const {
+      digitalWrite(pin, static_cast<uint8_t>(state));
+    }
+
+    uint8_t getPin() const {
+      return pin;
+    }
+};
+
 //Configure interrupt service routine
-void IRAM_ATTR SensorISR() {
+void IRAM_ATTR LedISR() {
   if (EndTime == 0) {
     EndTime = micros();
-    IsMeasuring = true;
+    isBlinking = true;
   }
 }
 
+static const Led Led1 (Config::Led1);
+static const Led Led2 (Config::Led2);
+static const Led Led3 (Config::Led3);
+
 void setup() {
-  Serial.begin(BaudRate);
-  pinMode(RelayPin, OUTPUT);
-  pinMode(SensorPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(SensorPin), SensorISR, FALLING);
+  Serial.begin(Config::BaudRate);
+  Led1.init();
+  Led2.init();
+  Led3.init();
+  attachInterrupt(digitalPinToInterrupt(Led1.getPin()), LedISR, FALLING);
 
   Serial.println("Getting ready...");
   delay(2000); // Wait for 2 seconds before starting the first measurement
@@ -67,7 +101,7 @@ void loop(){
     delay(1000); // Wait for 1 second before the next measurement
 
   } else if (count == TotalIterations) {
-    float average = (TotalDuration / (float)TotalIterations) / 1000.0;
+    float average = (TotalDuration / TotalIterations) / 1000.0;
     Serial.print("Average Duration: ");
     Serial.print(average);
     Serial.println(" ms");

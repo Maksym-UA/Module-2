59e711e6fe4c103f27989ff195b76de0b27bfe57
Maksym-UA <max.savin3@gmail.com>
2026-03-31 00:57:47 +0300

    Atomic count

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
+#include <atomic>
 
 struct Button {
   static const uint8_t ButtonPin = 16;
   volatile bool ButtonState;
-  volatile uint32_t NumberButtonPresses;
+  std::atomic<uint32_t> NumberButtonPresses{0};// Initialize atomic counter to 0
 };
 
-Button button1 = {false, 0};
+Button button1;
 
-unsigned long lastPrintTime = 0;
+
 
 void IRAM_ATTR handleButtonPress() {
   // Handle button press logic here
-  button1.NumberButtonPresses++;
+  button1.NumberButtonPresses.fetch_add(1, std::memory_order_relaxed); //atomic increment
   button1.ButtonState = true;
 }
 
@@ -25,10 +26,13 @@ void setup() {
 
 
 void loop() {
+    static unsigned long lastPrintTime = 0; // mentor  review
     unsigned long currentTime = millis();
+
     if (button1.ButtonState && (currentTime - lastPrintTime >= 50)) {
+      uint32_t presses = button1.NumberButtonPresses.load(std::memory_order_relaxed);
         Serial.print("Button pressed! Total presses: ");
-        Serial.println(button1.NumberButtonPresses);
+      Serial.println(presses);
         button1.ButtonState = false; // Reset the button state
         lastPrintTime = currentTime;
     }
